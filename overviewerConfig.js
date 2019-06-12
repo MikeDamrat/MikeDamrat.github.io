4cccdf87b03919d9fd1e864823bc706ec97c67b3
MikeDamrat <MikeDamrat@gmail.com>
2019-06-12 10:06:02 -0400

    New map!

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -10,7 +10,7 @@ var overviewerConfig = {
                 96
             ], 
             "isOverlay": false, 
-            "last_rendertime": 1560225752, 
+            "last_rendertime": 1560314127, 
             "name": "Overviewer Render (lighting)", 
             "poititle": "Markers", 
             "north_direction": 0, 
@@ -66,6 +66,6 @@ var overviewerConfig = {
             67, 
             94
         ], 
-        "cacheTag": "1560229157"
+        "cacheTag": "1560347652"
     }
 };
