a0764c41f4e3d05b28fb75835f5eb1e6bf535260
MikeDamrat <MikeDamrat@gmail.com>
2019-06-25 10:09:42 -0400

    New map - just updating existing map files

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -10,7 +10,7 @@ var overviewerConfig = {
                 96
             ], 
             "isOverlay": false, 
-            "last_rendertime": 1561005307, 
+            "last_rendertime": 1561432839, 
             "name": "Overviewer Render (lighting)", 
             "poititle": "Markers", 
             "north_direction": 0, 
@@ -66,6 +66,6 @@ var overviewerConfig = {
             67, 
             94
         ], 
-        "cacheTag": "1561016507"
+        "cacheTag": "1561446252"
     }
 };
