bc3d830f8bf646bdc2cb2d63306dd4907722850a
MikeDamrat <MikeDamrat@gmail.com>
2022-05-17 11:30:52 -0400

    new map!

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1652715206",
+        "cacheTag": "1652800455",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -49,7 +49,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "world",
-            "last_rendertime": 1652663880,
+            "last_rendertime": 1652774087,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
